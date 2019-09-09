32ea817630e0a994659799e4f282e0d831a3fd72
Ubuntu <ubuntu@ip-172-26-9-20.ap-northeast-2.compute.internal>
2019-09-09 03:16:23 +0000

    Updates

---
+++ precache-manifest.04e28fad08bb732969cc0aa8ef2bebf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94df7bc31fcbb0bcda3793b3df5deb33",
+    "url": "/jackthgu/index.html"
+  },
+  {
+    "revision": "581db8065ff36d1be876",
+    "url": "/jackthgu/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "49268a7ddf924362b4cd",
+    "url": "/jackthgu/static/js/2.fd03bb97.chunk.js"
+  },
+  {
+    "revision": "581db8065ff36d1be876",
+    "url": "/jackthgu/static/js/main.819ce262.chunk.js"
+  },
+  {
+    "revision": "29501610d7d2be011506",
+    "url": "/jackthgu/static/js/runtime~main.62867174.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jackthgu/static/media/logo.5d5d9eef.svg"
+  }
+]);
